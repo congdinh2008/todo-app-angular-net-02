d1d0e669b72fcbfdc3888efdc9088abbdd670b0b
Cong Dinh <congdinh2012>
2025-03-13 11:25:23 +0700

    Client: Refactor sidebar component with Font Awesome icons, update admin layout structure, and enhance responsiveness

--- src/modules/shared/common/sidebar/sidebar.component.ts
+++ src/modules/shared/common/sidebar/sidebar.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faAngleDoubleLeft, faAngleDoubleRight, faDashboard, faGear, faList, faUser, faUserShield } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  //#region Font Awesome Icons
+  public faDashboard: IconDefinition = faDashboard;
+  public faList: IconDefinition = faList;
+  public faGear: IconDefinition = faGear;
+  public faAngleDoubleLeft: IconDefinition = faAngleDoubleLeft;
+  public faAngleDoubleRight: IconDefinition = faAngleDoubleRight;
+  public faUser: IconDefinition = faUser;
+  public faUserShield: IconDefinition = faUserShield;
 
+  //#endregion
+  public isShowSidebar: boolean = false;
 }
--- src/modules/shared/layouts/admin-layout/admin-layout.component.ts
+++ src/modules/shared/layouts/admin-layout/admin-layout.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../common/header/header.component';
-import { FooterComponent } from '../../common/footer/footer.component';
 import { SidebarComponent } from '../../common/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css',
 })
