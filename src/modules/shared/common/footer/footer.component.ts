41c8967593b64b830947b361c26db074565af188
Cong Dinh <congdinh2012>
2025-03-13 10:53:35 +0700

    Step 8: Add Font Awesome icons to header and footer components, update layout structure

--- src/modules/shared/common/footer/footer.component.ts
+++ src/modules/shared/common/footer/footer.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faLocationDot, faPhone, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  //#region Font Awesome Icons
+  public faEnvelope: IconDefinition = faEnvelope;
+  public faPhone: IconDefinition = faPhone;
+  public faLocationDot: IconDefinition = faLocationDot;
 
+  public year: number = new Date().getFullYear();
 }
--- src/modules/shared/common/header/header.component.ts
+++ src/modules/shared/common/header/header.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FontAwesomeModule,
+  IconDefinition,
+} from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  //#region Font Awesome Icons
+  public faBars: IconDefinition = faBars;
 
+  //#endregion
+  public isShowProfileDropdown: boolean = false;
+  public isShowMainMenu: boolean = false;
 }
